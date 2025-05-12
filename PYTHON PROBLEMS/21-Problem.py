cd6e1178a11d0f88ab544e9c782caef99737b477
Virag Umraniya <irdgvaiiy@gmail.com>
2025-05-12 09:06:06 +0530

    21st code

--- PYTHON PROBLEMS/21-Problem.py
+++ PYTHON PROBLEMS/21-Problem.py
@@ -1,1 +1,6 @@
 # 21. Write a Python program to check if a year is a leap year.
+year = 2023
+if(year %4 == 0 or year %100 != 0 and year %400 == 0):
+    print(f"The {year} year is a leap year: ")
+else:
+    print(f"The {year} year is not a leap year: ")
